0f2001f6c5a9fbaeec9911be3037ba03a4acb7f4
Carla <cqc1999@gmail.com>
2026-05-19 15:45:20 +0200

    UserStore para CI

--- src/shared/stores/useUserStore.ts
+++ src/shared/stores/useUserStore.ts
@@ -1,24 +1,24 @@
 import { create } from 'zustand'
-import { User } from "../types/user";
+
+import { User } from '../types/user'
 
 interface UserStoreProps {
-    user: User | null
-    isLoading: boolean
+  user: User | null
+  isLoading: boolean
 }
 
 interface UserStore extends UserStoreProps {
-    setUser: (user: User | null) => void
-    setIsLoading: (isLoading: boolean) => void
+  setUser: (user: User | null) => void
+  setIsLoading: (isLoading: boolean) => void
 }
 
 const initialState: UserStoreProps = {
-    user: null,
-    isLoading: true
+  user: null,
+  isLoading: true,
 }
 
 export const useUserStore = create<UserStore>((set) => ({
-    ...initialState,
-
-    setUser: (user: User | null) => set(() => ({ user })),
-    setIsLoading: (isLoading: boolean) => set(() => ({ isLoading }))
+  ...initialState,
+  setUser: (user: User | null) => set(() => ({ user })),
+  setIsLoading: (isLoading: boolean) => set(() => ({ isLoading })),
 }))
